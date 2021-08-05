72e5abb5836c505e4e71b4cbf323d130d1c2716c
Ramon Metzker <ramonmetzker12@gmail.com>
2021-08-05 10:26:52 -0400

    exercicios aula 31

---
+++ aula31/index.js
@@ -0,0 +1,35 @@
+const verdadeira = true;
+
+// Let tem escopo de bloco { ... bloco }
+// Var só tem escopo de função
+
+// let nome = 'Ramon'; // criando
+// var nome2 = 'Ramon'; // criando
+
+// if (verdadeira) {
+//   let nome = 'Metzker'; // criando
+//   var nome2 = 'Silva'; // redeclarando
+
+//   if (verdadeira) {
+//     var nome2 = 'Jose'; // redeclarando
+//     let nome = 'Outra coisa';
+//   }
+// }
+
+// console.log(nome, nome2);
+
+// function falaOi () {
+
+//   if (verdadeira) {
+//     let nome = 'Ramon';
+//     var sobrenome = 'Metzker';
+//   }
+
+//   console.log(sobrenome);
+
+// }
+
+// falaOi();
+
+let sobrenome = "Metzker";
+console.log(sobrenome);
